dcd5ec5fde4acfe79c104ff837579e254d03b703
Kikols <francisco.lapamsilva@gmail.com>
2024-12-09 12:45:42 +0000

    andaimes meta4

--- src/codegen.c
+++ src/codegen.c
@@ -5,9 +5,102 @@
 #include "semantics.h"
 #include "codegen.h"
 
+const char *type_to_llvm3[] = TYPE_TO_LLVM;
+const enum type category_to_type3[] = CATEGORY_TO_TYPE;
+
 int temporary;   // sequence of temporary registers in a function
+int if_count = 0;
+int for_count = 0;
 
 
-void codegen_program(struct node *program) {
+void codegen_indent(int ind) {
+    for (int i=0; i<ind; i++) {
+        printf(" ");
+    }
+}
+
+
+
+void codegen_globalvar(struct node *node, struct symbol_list *symbol, int ind) {
+    codegen_indent(ind);
+    printf("%s @%s");
+}
+
+
+
+void codegen_funcheaderparams(struct node *params, struct symbol_list *symbol_scope, int ind) {
+    struct node *cur_param;
+    int curr = 0;
+    while((cur_param = getchild(params, curr++)) != NULL) {
+        struct node *type_node = getchild(cur_param, 0);
+        struct node *id_node = getchild(cur_param, 1);
+        if (curr > 1) {
+            printf(", ");
+        }
+        printf("%s %%%s", type_to_llvm3[category_to_type3[type_node->category]], id_node->token);
+    }
+}
+
+
+void codegen_funcheader(struct node *funcheader, struct symbol_list *symbol_scope, int ind) {
+
+    struct node *id_node, *type_node, *params_node;
+    id_node = getchild(funcheader, 0);
+    params_node = getchild(funcheader, 2);
+    if (params_node!=NULL) {
+        type_node = getchild(funcheader, 1);
+    } else {
+        type_node = NULL;
+        params_node = getchild(funcheader, 1);
+    }
+
+    codegen_indent(ind);
+    if (type_node!=NULL) {
+        printf("define %s @_%s(", type_to_llvm3[category_to_type3[type_node->category]], id_node->token);
+    } else {
+        printf("define void @_%s(", id_node->token);
+    }
+    codegen_funcheaderparams(params_node, symbol_scope, ind);
+    printf(")");
+}
+
+void codegen_funcbody(struct node *funcbody, struct symbol_list *symbol_scope, int ind) {
+
+}
+
+
+void codegen_function(struct node *node, struct symbol_list *symbol, int ind) {
+    // declare the function
+    struct symbol_list *func_scope = symbol->child_scope;
+
+    struct node *funcheader, *funcbody;
+    funcheader = getchild(node, 0);
+    funcbody = getchild(node, 1);
+
+    codegen_funcheader(funcheader, func_scope, ind);
+    printf("{\n");
+    codegen_funcbody(funcbody, func_scope, ind+1);
+    printf("}\n");
+}
+
+
+void codegen_program(struct node *program, struct symbol_list *table) {
+    printf("; bem fixe este programa\n");
+
+    // setup all the global variables
+    struct symbol_list *symbol;
+    for(symbol = table->next; symbol != NULL; symbol = symbol->next) {
+        if (!symbol->is_function) {
+            codegen_globalvar(symbol->node, symbol, 0);
+            printf("\n");
+        }
+    }
+
+    // setup all the functions
+    for(symbol = table->next; symbol != NULL; symbol = symbol->next) {
+        if (symbol->is_function) {
+            codegen_function(symbol->node, symbol, 0);
+        }
+    }
 
 }
--- src/codegen.h
+++ src/codegen.h
@@ -3,6 +3,10 @@
 
 #include "ast.h"
 
-void codegen_program(struct node *program);
+
+// type                 { bool_type,  int_type,  float32_type,  string_type,  undef_type,  none_type,  EMPTY     };   para referencia
+#define TYPE_TO_LLVM    { "i1",       "i32",     "double",      "i8*",        "ERROR_u",   "ERROR_n",  "ERROR_e" }
+
+void codegen_program(struct node *program, struct symbol_list *table);
 
 #endif
